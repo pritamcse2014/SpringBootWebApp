010199f5496af3cae91e3a97beeae5ef330bb51b
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-07-08 21:49:59 +0600

    RequestMapping

--- src/main/java/com/example/SpringBootWebApp/HomeController.java
+++ src/main/java/com/example/SpringBootWebApp/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.SpringBootWebApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
     public String Home() {
     	System.out.println("Home");
         return "index.jsp";
