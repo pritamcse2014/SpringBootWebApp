81f19cf39ead742f49f9ad06d0b34fac3d45ab10
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-07-18 21:33:10 +0600

    DisplayDataOnResultPage

--- src/main/java/com/example/SpringBootWebApp/HomeController.java
+++ src/main/java/com/example/SpringBootWebApp/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.SpringBootWebApp;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,12 +14,12 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String Add(HttpServletRequest request) {
+    public String Add(HttpServletRequest request, HttpSession session) {
         System.out.println("Add");
         int num1 = Integer.parseInt(request.getParameter("num1"));
         int num2 = Integer.parseInt(request.getParameter("num2"));
         int result = num1 + num2;
-        System.out.println(result);
+        session.setAttribute("result", result);
         return "result.jsp";
     }
 }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -6,7 +6,8 @@
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
-        <h2 class="title">Result is : </h2>
+        <h2 class="title">Result is : <%= session.getAttribute("result") %></h2>
+        <h2 class="title">Result is : ${result}</h2>
 
         <script src=""></script>
     </body>
